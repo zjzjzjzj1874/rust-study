8893a7daf7dc2b3521e349932bf4ef18453bfad2
zjzjzjzj <zjzjzjzj1874@gmail.com>
2022-01-25 11:38:17 +0800

    chore🔧($doc): add some learning rust study website
    Signed-off-by: zjzjzjzj <zjzjzjzj1874@gmail.com>

--- main.rs
+++ main.rs
@@ -1,6 +1,7 @@
 
 fn main() {
     // println!("hello {}!", "world")
+    // learning website: https://wiki.jikexueyuan.com/project/rust-primer/1st-glance/README.html
 
-    base::print_test();
+    use base::print_test();
 }
